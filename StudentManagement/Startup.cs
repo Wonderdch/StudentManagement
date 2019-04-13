64a84e3048b5eb4185f22226afe1333f3e07afc7
Wonderdch <476681765@qq.com>
2019-04-13 10:04:26 +0800

    配置 ASP.NET Core 请求处理管道

--- StudentManagement/Startup.cs
+++ StudentManagement/Startup.cs
@@ -1,33 +1,58 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace StudentManagement
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILogger<Startup> logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
+            app.Use(async (context, next) =>
+            {
+                context.Response.ContentType = "text/plain;charset=utf-8";
+
+                logger.LogInformation("MW1: 传入请求");
+                await next();
+                logger.LogInformation("MW1: 传出响应");
+            });
+
+            app.Use(async (context, next) =>
+            {
+                logger.LogInformation("MW2: 传入请求");
+                await next();
+                logger.LogInformation("MW2: 传出响应");
+            });
+
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                logger.LogInformation("MW3: 传入请求");
+                await context.Response.WriteAsync("MW3：处理请求，并生成响应");
+                logger.LogInformation("MW3: 传出响应");
             });
         }
     }
