f6bc0d58538e662a0b77727683ddbbc19e5809f3
Wonderdch <476681765@qq.com>
2019-04-24 19:20:32 +0800

    在 ASP.NET Core 中安装 MVC

---
+++ StudentManagement/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public string Index()
+        {
+            return "Hello World from MVC";
+        }
+    }
+}
