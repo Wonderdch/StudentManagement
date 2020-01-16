f5ff0f0a8cd48d3906d15a548638d1c6d2f75646
Wonderdch <476681765@qq.com>
2020-01-16 09:21:02 +0800

    记录异常信息

--- StudentManagement/Controllers/ErrorController.cs
+++ StudentManagement/Controllers/ErrorController.cs
@@ -1,11 +1,24 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace StudentManagement.Controllers
 {
     public class ErrorController : Controller
     {
+        private readonly ILogger<ErrorController> _logger;
+
+        /// <summary>
+        /// 通过依赖注入服务，注入 ILogger
+        /// 将指定类型的控制器作为泛型参数
+        /// </summary>
+        /// <param name="logger"></param>
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
+
         [Route("Error/{statusCode}")]
         public IActionResult HttpStatusCodeHandler(int statusCode)
         {
@@ -15,8 +28,11 @@ namespace StudentManagement.Controllers
             {
                 case 404:
                     ViewBag.ErrorMessage = "抱歉，您访问的页面不存在";
-                    ViewBag.Path = statusCodeResult.OriginalPath;
-                    ViewBag.QueryStr = statusCodeResult.OriginalQueryString;
+                    //ViewBag.Path = statusCodeResult.OriginalPath;
+                    //ViewBag.QueryStr = statusCodeResult.OriginalQueryString;
+
+                    _logger.LogWarning($"发生了一个 404 错误。\n路径：{statusCodeResult.OriginalPath}\n查询字符串：{statusCodeResult.OriginalQueryString}");
+
                     break;
 
             }
@@ -30,9 +46,11 @@ namespace StudentManagement.Controllers
         {
             var exceptionHandlerPathFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
 
-            ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
-            ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
-            ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
+            _logger.LogError($"路径：{exceptionHandlerPathFeature.Path}\n产生了一个错误：{exceptionHandlerPathFeature.Error}");
+
+            //ViewBag.ExceptionPath = exceptionHandlerPathFeature.Path;
+            //ViewBag.ExceptionMessage = exceptionHandlerPathFeature.Error.Message;
+            //ViewBag.StackTrace = exceptionHandlerPathFeature.Error.StackTrace;
 
             return View();
         }
