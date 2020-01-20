f7d22adaf0a842507adb9342f63a445f71f62623
Wonderdch <476681765@qq.com>
2020-01-20 16:29:35 +0800

    登录

--- StudentManagement/Controllers/AccountController.cs
+++ StudentManagement/Controllers/AccountController.cs
@@ -52,6 +52,30 @@ namespace StudentManagement.Controllers
             return View(model);
         }
 
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                ModelState.AddModelError(string.Empty, "登录失败，请重试");
+            }
+
+            return View(model);
+        }
+
         [HttpPost]
         public async Task<IActionResult> Logout()
         {
---
+++ StudentManagement/ViewModels/LoginViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "邮箱地址")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        [Display(Name = "记住我")]
+        public bool RememberMe { get; set; }
+    }
+}
