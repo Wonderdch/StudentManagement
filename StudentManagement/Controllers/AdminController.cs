bf007aa2d7ab2ff5bc82d3bc047598bca1d8b286
Wonderdch <476681765@qq.com>
2020-03-19 12:50:07 +0800

    编辑角色

--- StudentManagement/Controllers/AdminController.cs
+++ StudentManagement/Controllers/AdminController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using StudentManagement.Models;
 using StudentManagement.ViewModels;
 
 namespace StudentManagement.Controllers
@@ -10,9 +11,12 @@ namespace StudentManagement.Controllers
     {
         private readonly RoleManager<IdentityRole> _roleManager;
 
-        public AdminController(RoleManager<IdentityRole> roleManager)
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _roleManager = roleManager;
+            _userManager = userManager;
         }
 
         [HttpGet]
@@ -36,7 +40,7 @@ namespace StudentManagement.Controllers
 
                 if (result.Succeeded)
                 {
-                    return RedirectToAction("ListRoles", "Admin");
+                    return RedirectToAction("ListRoles");
                 }
 
                 foreach (var error in result.Errors)
@@ -53,6 +57,62 @@ namespace StudentManagement.Controllers
             var roles = _roleManager.Roles;
 
             return View(roles);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> EditRole(string id)
+        {
+            var role = await _roleManager.FindByIdAsync(id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色 Id 为 {id} 的信息不存在，请重试。";
+                return View("NotFound");
+            }
+
+            var model = new EditRoleViewModel
+            {
+                Id = role.Id,
+                RoleName = role.Name
+            };
+
+            var users = _userManager.Users.ToList();
+            foreach (var user in users)
+            {
+                // 如果用户拥有此角色，请讲用户名添加到 EditRoleViewModel 的 Users 属性中
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                {
+                    model.Users.Add(user.UserName);
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> EditRole(EditRoleViewModel model)
+        {
+            var role = await _roleManager.FindByIdAsync(model.Id);
+
+            if (role == null)
+            {
+                ViewBag.ErrorMessage = $"角色 Id 为 {model.Id} 的信息不存在，请重试。";
+                return View("NotFound");
+            }
+
+            role.Name = model.RoleName;
+            var result = await _roleManager.UpdateAsync(role);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListRoles");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View(model);
         }
     }
 }
---
+++ StudentManagement/ViewModels/EditRoleViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagement.ViewModels
+{
+    public class EditRoleViewModel
+    {
+        [Display(Name = "角色Id")]
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "角色名称")]
+        public string RoleName { get; set; }
+
+        public List<string> Users { get; set; }
+
+        public EditRoleViewModel()
+        {
+            Users = new List<string>();
+        }
+    }
+}
