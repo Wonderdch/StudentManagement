0f9f22115ea1e80d6d1778f128da60624ae61cee
Wonderdch <476681765@qq.com>
2020-04-11 18:26:49 +0800

    基于角色授权

---
+++ StudentManagement/Controllers/SomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentManagement.Controllers
+{
+    [Authorize(Roles = "Admin, User")]
+    public class SomeController : Controller
+    {
+        public string ABC()
+        {
+            return "ABC操作方法";
+        }
+
+        [Authorize(Roles = "Admin")]
+        public string XYZ()
+        {
+            return "XYZ操作方法";
+        }
+
+        [AllowAnonymous]
+        public string Anyone()
+        {
+            return "Anyone操作方法";
+        }
+    }
+}
